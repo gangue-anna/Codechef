09b13a51df7c2d725b881bd88dbe4fed358de7c8
Arvindh Vijayakumar <141946284+gangue-anna@users.noreply.github.com>
2025-08-07 12:42:28 +0530

    Updated little_elephants_candies.cpp

--- little_elephants_candies.cpp
+++ little_elephants_candies.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+	int T;
+	cin >> T;
+	
+	int N = 0, C = 0;
+	vector<int> arr;
+	
+	for (int i = 0; i < T; i++){
+	    cin >> N >> C;
+	    arr.clear();
+	    
+	    for (int j = 0; j < N; j++){
+	        int x;
+	        cin >> x;
+	        arr.push_back(x);
+	    }
+	    int sum = accumulate(arr.begin(), arr.end(), 0);
+	    
+	    if(sum <= C){
+	        cout << "Yes" << endl;
+	    }
+	    else{
+	        cout << "No" << endl;
+	    }
+	}
+
+}
